55de11f6f2c7d5b5e33e2669141087cdda9021fb
Oscar Beltran <oscar.beltran@masivian.com>
2020-05-10 02:42:02 -0500

    Add login request

---
+++ js/login.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	$("form").on("submit", function(e) {
+
+    	e.preventDefault()
+    	$email = $("#inputUsername")
+    	$password = $("#inputPassword")
+
+    	if ($email.val() === "" || $password.val() === "") {
+      		alert("Por favor ingresa todos tus datos")
+    	}
+	    
+	    userLogin = {
+	      email: $email.val(),
+	      password: $password.val()
+	    }
+
+	    $.ajax({
+	      type: "POST",
+	      url: 'http:localhost:8080/login',
+	      data: JSON.stringify(userLogin),
+	      success: function(data) {
+	        console.log("USUARIO LOGEADO");
+	      },
+	      dataType: 'json',
+	      contentType: "application/json"
+	    });
+  	})
+})
